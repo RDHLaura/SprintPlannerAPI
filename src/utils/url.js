6742657a91a9c63907e1a2968ac07370f8ebe3b2
rdhlaura <laurarodriguezdeharo@outlook.es>
2023-02-19 20:14:18 +0100

    dirección API y JSONs

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,11 +1,11 @@
 //devuelve la base de la url donde está desplegada la api
 const getBaseUrl = (req) => {
-  return "full url ==> ",req.protocol + '://' + req.get('host');
+  return "full url ==> ", 'https://' + req.get('host');
 }
 
 //devuelve la url completa de la api que se ha usado para la petición
 const getFullUrl = (req) => {
-  return req.protocol + '://' + req.get('host') + req.originalUrl.split("?").shift();
+  return 'https://' + req.get('host') + req.originalUrl.split("?").shift();
 }
 
 
